610f9a1c2d73dc332aaced15f78efda145533918
Woo-JongHo <jonghowoo33@gmail.com>
2023-11-22 22:01:18 +0900

    "작업" DB 설계, VO, Mapper, DBManager, sqlMapConfig 1차 작업 -CodeVO는 Parsing 형태 결정 후 값 넣을 것 "다음작업" -View 페이지 하나로 구글로그인으로 데이터 값 User Table에 받아오기.

---
+++ src/main/java/com/example/podofarm/vo/CodeVO.java
@@ -0,0 +1,7 @@
+package com.example.podofarm.vo;
+
+public class CodeVO {
+
+    //아직 Code를 어떻게 Parsing하는지 정해져있지 않기 때문에 틀만
+    
+}
---
+++ src/main/java/com/example/podofarm/vo/UserVO.java
@@ -0,0 +1,86 @@
+package com.example.podofarm.vo;
+
+
+
+public class UserVO {
+    private int userNo;
+    private String id;
+    private String password;
+    private String name;
+    private String gender;
+    private String phone;
+    private String study;
+    private int solved;
+    private String leader;
+
+    public int getUserNo() {
+        return userNo;
+    }
+    public void setUserNo(int userNo) {
+        this.userNo = userNo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStudy() {
+        return study;
+    }
+
+    public void setStudy(String study) {
+        this.study = study;
+    }
+
+    public int getSolved() {
+        return solved;
+    }
+
+    public void setSolved(int solved) {
+        this.solved = solved;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
+}
