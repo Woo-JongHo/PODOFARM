b133d6943a442aaa0a23ad3c0ad2bdd2c0e7db70
Woo-JongHo <jonghowoo33@gmail.com>
2024-04-08 17:07:08 +0900

    연동하기버튼으로 Data 넘기고 확인할 준비 완료

--- src/main/java/com/example/podofarm/controller/CodeController.java
+++ src/main/java/com/example/podofarm/controller/CodeController.java
@@ -1,10 +1,24 @@
 package com.example.podofarm.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
+@RestController
+@CrossOrigin(origins = "chrome-extension://ghbibjdmcondjdiebninoidgihdklndj")
 public class CodeController {
-    // 익스텐션에서 코드를 받아오는 공간
-    
+
+    @PostMapping("/receive-code")
+    public String receiveCode(@RequestBody String code) {
+        // 익스텐션으로부터 받은 코드를 처리하는 로직을 작성
+
+
+        System.out.println("Received code from extension: " + code);
+
+
+
+        // 응답을 반환할 수도 있음
+        return "Code received successfully";
+    }
 }
