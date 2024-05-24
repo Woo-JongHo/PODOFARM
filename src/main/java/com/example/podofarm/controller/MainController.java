d251950fed6bb630a9df6fafeba55ea114a972c9
Woo-JongHo <jonghowoo33@gmail.com>
2024-05-24 15:13:33 +0900

    현재 포도심기 페이지, 이중리프로 덮어쓰면 안됨 .

--- src/main/java/com/example/podofarm/controller/MainController.java
+++ src/main/java/com/example/podofarm/controller/MainController.java
@@ -20,11 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.TextStyle;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Random;
-import java.util.Map;
+import java.util.*;
 
 
 @Controller
@@ -55,7 +51,7 @@ public class MainController {
         StudyVO study = new StudyVO();
 
         String id = "123456";
-    //  String id = (String)session.getAttribute("id");
+        //  String id = (String)session.getAttribute("id");
 
 
         String StudyCode = StudyCode();
@@ -108,123 +104,107 @@ public class MainController {
     public String studyMainPage(Model model, @PathVariable("s_code") String s_code, HttpSession session) throws JsonProcessingException {
         String id = (String) session.getAttribute("id");
 
+        // 테스트용 고정값 설정
         id = "104539211393038791047";
         s_code = "423XDF";
 
-        System.out.println("세션값 확인" + " id 는 " + id + " 스터디 코드는 " + s_code);
+        System.out.println("세션값 확인: id는 " + id + " 스터디 코드는 " + s_code);
 
-        //01 만약 s_code가 없는 url이면 에러 페이지를 뜬다.
+        // 01 만약 s_code가 없는 url이면 에러 페이지를 뜬다.
         int findStudyCode = studyService.findStudyCode(s_code);
-
-        if(findStudyCode == -1){
-            // 페이지 에러로 다시 home으로
+        if (findStudyCode == -1) {
             return "redirect:/pf";
         }
 
-        //불러오는 데이터 목록
-
-
-
-        //  1. 스터디 명, 스터디 코드, 스터디 비밀번호 2. 남은 스터디 일 수 * 스터디 인원
+        // 불러오는 데이터 목록
         model.addAttribute("getStudyName", studyService.getStudyName(s_code));
         model.addAttribute("getTotalMember", studyService.getTotalMember(s_code));
         model.addAttribute("getStudyMember", studyService.getStudyMember(s_code));
+        model.addAttribute("getDday", studyService.getDday(s_code));
+        model.addAttribute("s_code", s_code);
+        model.addAttribute("id", id);
 
-        model.addAttribute("getDday",studyService.getDday(s_code));
-        model.addAttribute("getStudyMember", studyService.getStudyMember(s_code));
-
-        //03 스터디 남은 일 수 계산기
-        model.addAttribute("s_code", s_code); // 스터디 코드를 모델에 추가
-        model.addAttribute("id",id);
-
-
-        //포도농사 칸
-
-        //월 표기
+        // 포도농사 칸
         String getMonthName = getMonthName();
         model.addAttribute("getMonthName", getMonthName);
 
-        //해당 월 날짜
+        // 해당 월 날짜
         int DayCheck = DayCheck();
-        System.out.println("이번달" + DayCheck);
+        System.out.println("이번달: " + DayCheck);
         model.addAttribute("DayCheck", DayCheck);
 
-        //날짜마다 문제 푼 갯수 파악
-        //스터디 멤버를 가져오고, 멤버의 id를 가지고 와서
-        //getSolvedList로 가지고 온다
+        // 스터디 멤버를 가져오고, 멤버의 id를 가져와서
         model.addAttribute("getStudyMemberID", studyService.getStudyMemberID(s_code));
         System.out.println(studyService.getStudyMemberID(s_code));
         List<String> memberID = (List<String>) studyService.getStudyMemberID(s_code);
         int index = memberID.size();
-        
-        //멤버마다 푼 문제를 확인
-        for (int i = 0 ; i < index ; i++ ){
-            ArrayList<Map<String, String>> solvedList;
-            solvedList = codeService.getSolvedByDayCurrentMonth(memberID.get(i));
 
-            //solved 값만 가져온다
-            //1. DayCheck만큼 배열을 생성합니다.
-            //for 루프를 돌면서, C_DATE 값이 없으면 SUBSTRING으로 날짜를 추출하여 배열에다가 값을 더합니다
+        // 2차원 배열 선언
+        String[][] solvedDataTypeStrings = new String[index][DayCheck];
 
-            int [] solvedMonth = new int[DayCheck];
+        // 멤버마다 푼 문제를 확인
+        for (int i = 0; i < index; i++) {
+            ArrayList<Map<String, String>> solvedList = codeService.getSolvedByDayCurrentMonth(memberID.get(i));
+
+            // solved 값만 가져온다
+            int[] solvedMonth = new int[DayCheck];
             for (Map<String, String> map : solvedList) {
                 String dataDay = map.get("C_DATE");
-                int day = Integer.parseInt(dataDay.substring(3,5));
+                int day = Integer.parseInt(dataDay.substring(3, 5));
                 String solved = String.valueOf(map.get("SOLVED"));
-                solvedMonth[day-1] = Integer.parseInt(solvedMonth[day-1] + solved);
+                solvedMonth[day - 1] += Integer.parseInt(solved);
             }
 
-
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            String[] solvedDataTypeString = new String[DayCheck];
             for (int j = 0; j < DayCheck; j++) {
                 switch (solvedMonth[j]) {
                     case 0:
-                        solvedDataTypeString[j] = "solved-0";
+                        solvedDataTypeStrings[i][j] = "solved-0";
                         break;
                     case 1:
-                        solvedDataTypeString[j] = "solved-1";
+                        solvedDataTypeStrings[i][j] = "solved-1";
                         break;
                     case 2:
-                        solvedDataTypeString[j] = "solved-2";
+                        solvedDataTypeStrings[i][j] = "solved-2";
                         break;
                     case 3:
-                        solvedDataTypeString[j] = "solved-3";
+                        solvedDataTypeStrings[i][j] = "solved-3";
                         break;
                     default:
-                        solvedDataTypeString[j] = "solved-4";
+                        solvedDataTypeStrings[i][j] = "solved-4";
                         break;
                 }
             }
-
-            String solvedDataConvertJson = objectMapper.writeValueAsString(solvedDataTypeString);
-            System.out.println(solvedDataConvertJson + "JSON 변환 확인");
-            model.addAttribute("solvedData",solvedDataTypeString);
         }
 
+        // 2차원 배열의 각 요소를 모델에 추가하고 출력
+        for (int k = 0; k < index; k++) {
+            model.addAttribute("solvedData" + (k + 1), solvedDataTypeStrings[k]);
+            System.out.println("solvedData 확인용 solvedData" + (k + 1) + ": " + Arrays.toString(solvedDataTypeStrings[k]));
+        }
+
+        model.addAttribute("memberSize", index);
 
         return "ver4/main";
     }
 
 
-        //날짜 입력란. 이번달의 일수를 확인
-        public int DayCheck(){
-            LocalDate currentDate = LocalDate.now();
-            YearMonth yearMonth = YearMonth.of(currentDate.getYear(), currentDate.getMonth());
-            int daysInMonth = yearMonth.lengthOfMonth();
+    //날짜 입력란. 이번달의 일수를 확인
+    public int DayCheck(){
+        LocalDate currentDate = LocalDate.now();
+        YearMonth yearMonth = YearMonth.of(currentDate.getYear(), currentDate.getMonth());
+        int daysInMonth = yearMonth.lengthOfMonth();
 
-            System.out.println("현재 " + currentDate.getMonthValue() + "월은 " + daysInMonth + "일입니다.");
+        System.out.println("현재 " + currentDate.getMonthValue() + "월은 " + daysInMonth + "일입니다.");
 
-            return daysInMonth;
-        }
+        return daysInMonth;
+    }
 
-        public String getMonthName() {
-            LocalDate currentDate = LocalDate.now();
-            YearMonth yearMonth = YearMonth.of(currentDate.getYear(), currentDate.getMonth());
-            String monthName = yearMonth.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+    public String getMonthName() {
+        LocalDate currentDate = LocalDate.now();
+        YearMonth yearMonth = YearMonth.of(currentDate.getYear(), currentDate.getMonth());
+        String monthName = yearMonth.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
 
-            return monthName;
-        }
+        return monthName;
+    }
 }
 
